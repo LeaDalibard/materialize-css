c499a01f54180b5cfdc9d107c278cee37b877c75
Lea Dalibard <leadalibard@gmail.com>
2021-02-05 12:06:54 +0100

    Add Carrousel

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.carousel').carousel({
+        fullWidth: true,
+        indicators: true,
+    });
 });
